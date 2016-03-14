4377a5fbb1c332b237e509c9b30ddb1d9d775d79
songqiang0521 <songqiang0521@163.com>
2016-03-14 22:32:56 +0800

    add test_build
    ...

---
+++ Test_Build/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test_Build
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("good");
+            Console.WriteLine("good");
+
+        }
+    }
+}
